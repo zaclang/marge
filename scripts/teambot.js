b53c1d87ccc5661a83542bfcf3d6145d41e8f5f1
Zac Lang <zac.wr.lang@gmail.com>
2017-02-28 22:14:22 +1100

    test exclude self & last reviewer

--- scripts/teambot.js
+++ scripts/teambot.js
@@ -20,5 +20,4 @@ module.exports = (robot) => {
   robot.respond(/(add)(.*)/i, teamBot.add.bind(teamBot));
   robot.respond(/(remove)(.*)/i, teamBot.remove.bind(teamBot));
   robot.respond(/(list)(.*)/i, teamBot.list.bind(teamBot));
-  robot.respond(/(who am i)(.*)/i, teamBot.whoami.bind(teamBot));
 };
