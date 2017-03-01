74b35720b769a562a661aa24307a3c34feec7451
Zac Lang <zac.wr.lang@gmail.com>
2017-03-01 21:09:49 +1100

    removed leftover bind:

--- scripts/teambot.js
+++ scripts/teambot.js
@@ -29,6 +29,5 @@ module.exports = (robot) => {
   robot.respond(/(skip)(.*)/i, teamBot.skip.bind(teamBot));
   robot.respond(/(rewind)(.*)/i, teamBot.rewind.bind(teamBot));
   robot.respond(/(current)(.*)/i, teamBot.current.bind(teamBot));
-  robot.respond(/(say)(.*)/i, teamBot.say.bind(teamBot));
   robot.respond(/(set)(.*)/i, teamBot.set.bind(teamBot));
 };
