c954a7fa045bed9b0aa3b68efedf21d1431becc9
Zac Lang <zac.wr.lang@gmail.com>
2017-05-09 20:18:43 +1000

    initial webhook test

--- scripts/start.js
+++ scripts/start.js
@@ -19,8 +19,6 @@ module.exports = (robot) => {
 
   let teamBot = new TeamBot(robot);
 
-  // console.log('ROBOT:', robot.adapterName, 'MAP:', _.flatMap(robot));
-
   robot.hear(/.*github.com\/crimson-education\/.*\/pull/i, teamBot.delegate.bind(teamBot));
   robot.hear(/welcome back @?marge/i, teamBot.welcomeBack.bind(teamBot));
   robot.respond(/(add)(.*)/i, teamBot.add.bind(teamBot));
@@ -31,4 +29,21 @@ module.exports = (robot) => {
   robot.respond(/(current)(.*)/i, teamBot.current.bind(teamBot));
   robot.respond(/(set)(.*)/i, teamBot.set.bind(teamBot));
   robot.respond(/(version)(.*)/i, teamBot.version.bind(teamBot));  
+  robot.router.post('/incoming', (req, res) => {
+    let data    = req.body.payload != null ? JSON.parse(req.body.payload) : req.body;
+    let room    = data.room;
+    let message = data.message;
+
+    if (typeof room !== 'string' || typeof message === 'undefined') {
+      res.send(422); return;
+    }
+
+    if (typeof message === 'string') {
+      robot.messageRoom(room, message);
+    } else if (message instanceof Array) {
+      message.forEach(line => robot.messageRoom(room, line));
+    }
+
+    res.send(200);
+  });
 };
