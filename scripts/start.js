e212cf0f920fde35e031d8fbe90260eadffefb0b
Zac Lang <zac.wr.lang@gmail.com>
2017-05-09 20:38:09 +1000

    parse pr

--- scripts/start.js
+++ scripts/start.js
@@ -29,21 +29,5 @@ module.exports = (robot) => {
   robot.respond(/(current)(.*)/i, teamBot.current.bind(teamBot));
   robot.respond(/(set)(.*)/i, teamBot.set.bind(teamBot));
   robot.respond(/(version)(.*)/i, teamBot.version.bind(teamBot));  
-  robot.router.post('/incoming', (req, res) => {
-    let data    = req.body.payload != null ? JSON.parse(req.body.payload) : req.body;
-    let room    = data.room;
-    let message = data.message;
-
-    if (typeof room !== 'string' || typeof message === 'undefined') {
-      res.send(422); return;
-    }
-
-    if (typeof message === 'string') {
-      robot.messageRoom(room, message);
-    } else if (message instanceof Array) {
-      message.forEach(line => robot.messageRoom(room, line));
-    }
-
-    res.send(200);
-  });
+  robot.router.post('/webook', teamBot.webook.bind(teambot));
 };
