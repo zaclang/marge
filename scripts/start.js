e9871ea469c5e2330ad923bff47a321e3018898f
Zac Lang <zac.wr.lang@gmail.com>
2017-05-10 21:28:22 +1000

    move webhook res up

--- scripts/start.js
+++ scripts/start.js
@@ -30,5 +30,20 @@ module.exports = (robot) => {
   robot.respond(/(current)(.*)/i, teamBot.current.bind(teamBot));
   robot.respond(/(set)(.*)/i, teamBot.set.bind(teamBot));
   robot.respond(/(version)(.*)/i, teamBot.version.bind(teamBot));  
-  robot.router.post('/webook', (req, res) => githubEventParser.handler);
+  
+  robot.router.post('/webook', (req, res) => {
+    let data = req.body.payload != null ? JSON.parse(req.body.payload) : req.body; 
+    let room = 'testing';  
+    let message = data.pull_request.url;
+
+    if (typeof room !== 'string' || typeof message === 'undefined') {
+      res.send(422); return;
+    }
+
+    // if (typeof message === 'string') {
+    //   robot.messageRoom(room, message);
+    // }
+
+    res.send(message);       
+  });
 };
