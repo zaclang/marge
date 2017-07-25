9090b419a2c2f5a1f6c8adf772a9268929eeadf1
Zac Lang <zac.wr.lang@gmail.com>
2017-07-25 20:53:19 +1000

    removed skip method

--- scripts/start.js
+++ scripts/start.js
@@ -25,7 +25,7 @@ module.exports = (robot) => {
   robot.respond(/(add)(.*)/i, teamBot.add.bind(teamBot));
   robot.respond(/(remove)(.*)/i, teamBot.remove.bind(teamBot));
   robot.respond(/(list)(.*)/i, teamBot.list.bind(teamBot));
-  robot.respond(/(skip)(.*)/i, teamBot.skip.bind(teamBot));
+  robot.respond(/(skip)(.*)/i, teamBot.delegate.bind(teamBot));
   robot.respond(/(rewind)(.*)/i, teamBot.rewind.bind(teamBot));
   robot.respond(/(current)(.*)/i, teamBot.current.bind(teamBot));
   robot.respond(/(set)(.*)/i, teamBot.set.bind(teamBot));
