512f776b98442f1f6961298bde4cd374651ea662
Zac Lang <zac.wr.lang@gmail.com>
2017-08-28 18:05:06 +1000

    move hardcoded phrases to /data

---
+++ data/responses.js
@@ -0,0 +1,11 @@
+export default [
+	'start reviewing, or start running...',
+	'fear me.',
+	'pull your weight!',
+	'do something or weep.',
+	`don\`t make me come over there!`,
+	`my patience is not to be tested.`,
+	'the clock is ticking...',
+	'start moving!',
+	'$%##$ $#%@#&!@!',      
+];
