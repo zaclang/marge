7f2ba9e20537a8976b434c77c30c5b274d9c7561
Zac Lang <zac.wr.lang@gmail.com>
2017-08-31 20:05:40 +1000

    comments to highlight potential modules

--- scripts/start.js
+++ scripts/start.js
@@ -19,34 +19,23 @@ import Marge from "../lib/Marge";
 module.exports = robot => {
   let marge = new Marge(robot);
 
-  robot.hear(/.*github.com\/.*\/.*\/pull/i, marge.delegate.bind(marge));
+  // misc
+  robot.respond(/(version)(.*)/i, marge.version.bind(marge));
+
+  // user management
   robot.hear(/welcome back @?marge/i, marge.welcomeBack.bind(marge));
   robot.respond(/(add)(.*)/i, marge.add.bind(marge));
   robot.respond(/(remove)(.*)/i, marge.remove.bind(marge));
   robot.respond(/(list)(.*)/i, marge.list.bind(marge));
+  robot.respond(/(reset)/gi, marge.reset.bind(marge));  
+  
+  // github
+  robot.hear(/.*github.com\/.*\/.*\/pull/i, marge.delegate.bind(marge));
   robot.respond(/(skip)(.*)/i, marge.delegate.bind(marge));
   robot.respond(/(rewind)(.*)/i, marge.rewind.bind(marge));
   robot.respond(/(current)(.*)/i, marge.current.bind(marge));
   robot.respond(/(assign|set)(.*)/i, marge.delegate.bind(marge));
-  robot.respond(/(version)(.*)/i, marge.version.bind(marge));
-  robot.respond(/(advise|advice)( for )?(.*)?$/i, marge.advice.bind(marge));
-  robot.respond(/(reset)/gi, marge.reset.bind(marge));
-
-  //robot.router.post('/webook', (req, res) => {
-  //  let data = req.body.payload != null ? JSON.parse(req.body.payload) : req.body;
-  //  let room = 'testing';
-  //  let message = data.pull_request.url;
-  //
-  //
-  //
-  //  if (typeof room !== 'string' || typeof message === 'undefined') {
-  //    res.send(422); return;
-  //  }
-  //
-  //  // if (typeof message === 'string') {
-  //  //   robot.messageRoom(room, message);
-  //  // }
-  //
-  //  res.send(message);
-  //});
+  
+  // advice
+  robot.respond(/(advise|advice)( for )?(.*)?$/i, marge.advice.bind(marge));  
 };
